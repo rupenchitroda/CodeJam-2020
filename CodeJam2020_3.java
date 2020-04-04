7628b93b048f8dc6dde8fe6e07ba30b30a258454
Rupen Chitroda <42803653+rupenchitroda@users.noreply.github.com>
2020-04-04 23:18:17 +0530

    Update CodeJam2020_3.java

--- CodeJam2020_3.java
+++ CodeJam2020_3.java
@@ -18,69 +18,65 @@ public class Solution {
         sc.nextLine();
 
         while(t-- > 0){
-            solve();
-        }
-    }
+            int n = sc.nextInt();
+            int[][] mat = new int[n][2];
+            int[][] matSorted = mat.clone();
+            char person = 'J';
+            char[] chars = new char[n];
+            Stack<int[]> JStack = new Stack<>();
+            Stack<int[]> CStack = new Stack<>();
+            boolean impossible = false;
 
-    private static void solve(){
-        int n = sc.nextInt();
-        int[][] mat = new int[n][2];
-        int[][] matSorted = mat.clone();
-        char person = 'J';
-        char[] chars = new char[n];
-        Stack<int[]> JStack = new Stack<>();
-        Stack<int[]> CStack = new Stack<>();
-        boolean impossible = false;
+            Map<int[], Integer> map = new HashMap<>();
 
-        Map<int[], Integer> map = new HashMap<>();
-
-        for (int i=0; i < mat.length; i++) {
-            for(int j=0; j < mat[i].length; j++) {
-                mat[i][j] = sc.nextInt();
+            for (int i=0; i < mat.length; i++) {
+                for(int j=0; j < mat[i].length; j++) {
+                    mat[i][j] = sc.nextInt();
+                }
+                map.put(mat[i], i);
             }
-            map.put(mat[i], i);
-        }
 
-        Arrays.sort(matSorted, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] a, int[] b) {
-                return a[0]-b[0];
-            }
-        });
+            Arrays.sort(matSorted, new Comparator<int[]>() {
+                @Override
+                public int compare(int[] a, int[] b) {
+                    return a[0]-b[0];
+                }
+            });
 
-        for(int i=0; i<matSorted.length; i++) {
-            chars[map.get(matSorted[i])] = person;
+            for(int i=0; i<matSorted.length; i++) {
+                chars[map.get(matSorted[i])] = person;
 
-            if(i < matSorted.length - 1 && doesOverlap(matSorted[i], matSorted[i+1])) {
-                if(person == 'J'){
-                    JStack.push(matSorted[i]);
-                    person = getPerson(person); 
+                if(i < matSorted.length - 1 && doesOverlap(matSorted[i], matSorted[i+1])) {
+                    if(person == 'J'){
+                        JStack.push(matSorted[i]);
+                        person = getPerson(person); 
 
-                    if(!(CStack.isEmpty()) && doesOverlap(CStack.peek(), matSorted[i+1])) {
-                        impossible = true;
-                        break;
+                        if(!(CStack.isEmpty()) && doesOverlap(CStack.peek(), matSorted[i+1])) {
+                            impossible = true;
+                            break;
+                        }
+
+
+                    } else {
+                        CStack.push(matSorted[i]);
+                        person = getPerson(person);
+
+                        if(!(JStack.isEmpty()) && doesOverlap(JStack.peek(), matSorted[i+1])) {
+                            impossible = true;
+                            break;
+                        }
                     }
-                
-               
                 } else {
-                    CStack.push(matSorted[i]);
-                    person = getPerson(person);
-
-                    if(!(JStack.isEmpty()) && doesOverlap(JStack.peek(), matSorted[i+1])) {
-                        impossible = true;
-                        break;
+                    if(person == 'J') {
+                        JStack.push(matSorted[i]);
+                    } else {
+                        CStack.push(matSorted[i]);
                     }
                 }
-            } else {
-                if(person == 'J') {
-                    JStack.push(matSorted[i]);
-                } else {
-                    CStack.push(matSorted[i]);
-                }
             }
-        }
 
-        System.out.println("Case #" + (tn++) + ": " + (impossible ? "IMPOSSIBLE" : new String(chars)));
+            System.out.println("Case #" + (tn++) + ": " + (impossible ? "IMPOSSIBLE" : new String(chars)));
+        }
     }
 
     private static char getPerson(char p) {
